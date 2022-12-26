5026d2fa4391ee1497267d18b54cf24552662674
LucaAdorni <luca.adorni.98@gmail.com>
2022-12-26 20:43:54 +0100

    fix: adjusted sh code

--- codes/3_URL_unshortening.sh
+++ codes/3_URL_unshortening.sh
@@ -1,7 +1,7 @@
 #PBS -N polpo /// this is just the name of our job
 #PBS -j oe /// how the standard output is collected, it is just a log filed
 #PBS -V
-#PBS -l procs=50,mem=50gb /// specify the resources we need to run
+#PBS -l procs=50,mem=100gb /// specify the resources we need to run
 
 #Move into the directory you submitted from
 cd $PBS_O_WORKDIR
@@ -13,4 +13,4 @@ module load python/anaconda3
 source activate polpo
 
 #$PBS_NODFILE tells mpirun which CPU's PBS reserved for the job
-mpirun -n 50 -machinefile $PBS_NODEFILE python 3_URL_unshortening_mpi.py
+mpirun -n 50 -machinefile $PBS_NODEFILE python ./polpo/codes/3_URL_unshortening_mpi.py
