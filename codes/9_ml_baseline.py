d26a07bc7572dcca42afaff150d1f11d34bfbb3c
LucaAdorni <luca.adorni.98@gmail.com>
2023-01-21 18:24:33 -0600

    fix: removed table for results

--- codes/9_ml_baseline.py
+++ codes/9_ml_baseline.py
@@ -101,7 +101,7 @@ model_dict = {
     #,'lightgbm': lightgbm.LGBMRegressor(random_state = random_seed, n_jobs = -1)
     , 'lasso': Lasso(random_state = random_seed)
     , 'catboost': CatBoostRegressor(random_state = random_seed)
-    , 'xgboost': xgb.XGBRegressor(random_state = random_seed, n_jobs = -1)
+    #, 'xgboost': xgb.XGBRegressor(random_state = random_seed, n_jobs = -1)
 }
 
 # PARAMETERS FOR LOGISTIC REGRESSION -------
@@ -234,54 +234,3 @@ with open(f'{path_to_results}val_results{tune_tag}.pickle', 'wb') as handle:
     pickle.dump(results_val, handle)
 with open(f'{path_to_results}test_results{tune_tag}.pickle', 'wb') as handle:
     pickle.dump(results_test, handle)
-
-
-# # Get latex code for the results
-# with open(f'{path_to_results}test_results{tune_tag}.pickle', 'rb') as handle:
-#     results_test = pickle.load(handle)
-
-
-# model_dict = {
-#     'rand_for': RandomForestRegressor(random_state = random_seed, n_jobs = -1)
-#     #,'lightgbm': lightgbm.LGBMRegressor(random_state = random_seed, n_jobs = -1)
-#     , 'lasso': Lasso(random_state = random_seed)
-#     , 'catboost': CatBoostRegressor(random_state = random_seed)
-# }
-
-# method_list = ['frequency', 'onehot','tf_idf']
-# def get_results(results_dict):
-#     mae = []
-#     mse = []
-#     r2 = []
-#     for estimator in model_dict.keys():
-#         mae_results = [estimator]
-#         mse_results = [estimator]
-#         r2_results = [estimator]
-#         for method in method_list:
-#             mae_results.append(results_dict[method][estimator]["MAE"])
-#             mse_results.append(results_dict[method][estimator]["MSE"])
-#             r2_results.append(results_dict[method][estimator]["R2"])
-#         mae.append(mae_results)
-#         mse.append(mse_results)
-#         r2.append(r2_results)
-#     return mae, mse, r2
-
-# mae_test, mse_test, r2_test = get_results(results_test)
-
-# print(tabulate(mae_test))
-
-
-# def get_results(results_dict):
-#     final_results = []
-#     for estimator in model_dict.keys():
-#         for method in method_list:
-#             final_results.append([estimator, method, "No", results_dict[method][estimator]["MAE"], results_dict[method][estimator]["MSE"], results_dict[method][estimator]["R2"]])
-#     return final_results
-
-# final_res = get_results(results_test)
-
-
-# table = tabulate(final_res, tablefmt = 'latex', headers = ['Model', 'Method', 'Tuning', 'MAE', "MSE", "R2"], floatfmt=".4f")
-
-# with open(f'{path_to_tables}table_3_ml_baseline.tex', 'w') as f:
-#     f.write(table)
