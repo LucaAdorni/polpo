4510de8bde7a7afad38865a4aac2af0cccb4dd8e
LucaAdorni <luca.adorni.98@gmail.com>
2023-02-04 17:14:43 -0600

    feat: finished predicting

--- codes/10_predict_pol.py
+++ codes/10_predict_pol.py
@@ -18,36 +18,15 @@ import matplotlib.pyplot as plt
 from tabulate import tabulate
 import dill
 import re
-from unidecode import unidecode
-import emoji
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
-from nltk import word_tokenize
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-import seaborn as sns
-import matplotlib.pyplot as plt
-from tabulate import tabulate
-import dill
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
-from catboost import CatBoostRegressor
-import lightgbm
-import xgboost as xgb
+import torch
+from torch.utils.data import DataLoader, Dataset, RandomSampler, random_split
+import transformers
+from transformers import AutoTokenizer, AutoModel, AutoConfig
+from typing import Callable, Dict, Generator, List, Tuple, Union
+from pathlib import PurePosixPath
+import itertools
+from torch import nn
+from tqdm import tqdm
 
 try:
     # Setup Repository
@@ -76,14 +55,27 @@ path_to_processed = f"{path_to_data}processed/"
 random_seed = 42
 random.seed(random_seed)
 
-estimator = 'catboost'
-method = 'frequency'
-tune_models = True
+bert_tag = '_256'
+percentage_filter = 0.4
+learning_rate = 3e-05
+batch_size = 32
+epoch_num = 0
+chunksize = 256
 
-if tune_models:
-    tune_tag = '_tuned'
+
+merge_tweets = True # set to True if we want to merge all tweets
+only_politics = False # set to True if we want to keep only politic tweets
+
+if only_politics:
+    politics_tag = '_politics'
 else:
-    tune_tag = ''
+    politics_tag = ''
+
+if merge_tweets:
+    merged_tag = '_merged'
+else:
+    merged_tag = '_individual'
+
 # ----------------------
 
 # 1. Import our dataset -------------------------------------------------
@@ -126,122 +118,185 @@ except:
     # Merge the tweets
     post_df = pd.concat([post_df, pre_df])
     pred = post_df.groupby(['scree_name', 'week_start']).tweet_text.apply(' '.join).reset_index()
-    del post_df, pre_df
+    
     # Drop any duplicates
     pred.drop_duplicates(subset = ['scree_name', 'week_start'], inplace = True)
+
+    # Get only user info
+    user_info = post_df[['user_description', 'scree_name', 'regions', 'age', 'gender', 'is_org']]
+    # drop duplicates
+    user_info.drop_duplicates(subset = 'scree_name',inplace = True)
+    assert user_info.shape[0] == post_df.scree_name.nunique()
+    del post_df, pre_df
+    # Now merge it with our pre-covid scrape
+    pred = pred.merge(user_info, on = 'scree_name', how = 'outer', validate = 'm:1', indicator = True)
+    assert pred._merge.value_counts()['left_only'] == 0
+
+    # we drop unmerged values
+    pred = pred.loc[pred._merge == 'both']
+    pred.drop(columns = '_merge', inplace = True)
 
     # Sort all the values
     pred.sort_values(by= ['scree_name', 'week_start'], inplace = True)
 
     pred.to_pickle(f"{path_to_processed}df_processed.pkl.gz", compression = 'gzip')
 
-#     # Vectorize our datasets -------------------------------------------------
+# 2. Load Our BERT Model ---------------------------------------------------------------------
 
-MAX_FEATURES = 10000 # maximum number of features
-min_df = 5 # minimum frequency
-max_df = 0.8 # maximum frequency
-N_GRAM = (1,2) # n_gram range
+def padd_split(tokens, label, index, percentage_filter = 1):
+  # split into chunks of 510 tokens, we also convert to list (default is tuple which is immutable)
+  input_id_chunks = list(tokens['input_ids'][0].split(chunksize - 2))
+  mask_chunks = list(tokens['attention_mask'][0].split(chunksize - 2))
 
-STOPWORDS = stopwords.words("italian")
-# we initialize our stemmer
-stemmer = SnowballStemmer("italian", ignore_stopwords=True)
+  final_token = []
+  # loop through each chunk
+  for i in range(len(input_id_chunks)):
+      single_token = {}
+      # add CLS and SEP tokens to input IDs
+      input_id_chunks[i] = torch.cat([
+          torch.tensor([101]), input_id_chunks[i], torch.tensor([102])
+      ])
+      # add attention tokens to attention mask
+      mask_chunks[i] = torch.cat([
+          torch.tensor([1]), mask_chunks[i], torch.tensor([1])
+      ])
+      # get required padding length
+      pad_len = chunksize - input_id_chunks[i].shape[0]
+      # check if tensor length satisfies required chunk size
+      if pad_len > 0:
+          # if padding length is more than 0, we must add padding
+          input_id_chunks[i] = torch.cat([
+              input_id_chunks[i], torch.Tensor([0] * pad_len)
+          ]).int()
+          mask_chunks[i] = torch.cat([
+              mask_chunks[i], torch.Tensor([0] * pad_len)
+          ]).int()
+      single_token['input_ids'] = input_id_chunks[i]
+      single_token['attention_mask'] = mask_chunks[i]
+      single_token['label'] = label
+      single_token['index'] = index
+      
+      if percentage_filter <1 and (single_token['attention_mask'].sum()/len(single_token['attention_mask'])).item() >= percentage_filter:
+        final_token.append(single_token)
+      elif percentage_filter == 1:
+        final_token.append(single_token)
+  return final_token
 
-def text_prepare(text) :
-    """
-        text: a string        
-        return: modified initial string
-    """
-        
-    text = text.lower() # lowercase text
-    text = emoji.demojize(text) # convert emojis to text
-    text = unidecode((text))
-    text = re.sub("#|@", "", text) # take away hashtags or mentions but keep the word
-    text = re.sub(r'(@[A-Za-z0–9_]+)|[^\w\s]|#|http\S+', "", text)
-    text =  " ".join([x for x in text.split()if x not in STOPWORDS]) # delete stopwords from text
-    text =  " ".join([stemmer.stem(x) for x in text.split()])
-    text =  " ".join([x for x in text.split()])
-    return text
+class Dataset(torch.utils.data.Dataset):
 
-# Load back the training set
-train = pd.read_pickle(f"{path_to_processed}train_clean.pkl.gz", compression = 'gzip')
+    def __init__(self, df, percentage_filter = 1):
+        tokenizer = AutoTokenizer.from_pretrained("m-polignano-uniba/bert_uncased_L-12_H-768_A-12_italian_alb3rt0")
 
-def vectorize_to_dataframe(df, vectorizer_obj):
-    """
-    Function to return a dataframe from our vectorizer results
-    """
-    df = pd.DataFrame(data = df.toarray(), columns = vectorizer_obj.get_feature_names())
-    return df
+        if merge_tweets:
+            df = df.reset_index()
+            self.texts = [padd_split(tokenizer.encode_plus(text, add_special_tokens = False, return_tensors="pt"), label, index, percentage_filter = percentage_filter) for text, label, index in zip(df['tweet_text'], df['final_polarization'], df.index)]
+            self.texts = [token for text in self.texts for token in text]
+            self.labels = [dictionary["label"] for dictionary in self.texts]
+            self.index = [dictionary["index"] for dictionary in self.texts]
+        else:
+            self.texts = [tokenizer(text, padding='max_length', max_length = 128, truncation=True, return_tensors="pt") for text in df['tweet_text']]
+            self.labels = [label for label in df['final_polarization']]
+            self.index = [index for index in df.index]
 
-def vectorize_features(X_train, X_test, method = 'frequency', include_val = False, X_val = ''):
-    """
-    Function to perform vectorization of our test sets
-    X_train, X_test, X_val: our dataframes
-    method: either 'frequency', 'tf_idf', 'onehot' to employ a different BoW technique
-    include_val: set to True if we also have a validation dataset
-    """
-    # initialize our vectorizer
-    if method == 'tf_idf':
-        vectorizer = TfidfVectorizer(ngram_range=N_GRAM, min_df=min_df, max_df=max_df, max_features=MAX_FEATURES)
-    elif method == 'frequency':
-        vectorizer = CountVectorizer(ngram_range=N_GRAM, min_df=min_df, max_df=max_df, max_features=MAX_FEATURES)
-    elif method == 'onehot':
-        vectorizer = CountVectorizer(ngram_range=N_GRAM, min_df=min_df, max_df=max_df, max_features=MAX_FEATURES, binary = True)
-        
-    X_train = vectorizer.fit_transform(X_train.text_final)
-    X_train = vectorize_to_dataframe(X_train, vectorizer)
-    X_test = vectorizer.transform(X_test.text_final)
-    X_test = vectorize_to_dataframe(X_test, vectorizer)
-    if include_val: 
-        X_val = vectorizer.transform(X_val.text_final)
-        X_val = vectorize_to_dataframe(X_val, vectorizer)
-    return X_train, X_test, X_val
+    def classes(self):
+        return self.labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_batch_labels(self, idx):
+        # Fetch a batch of labels
+        return np.array(self.labels[idx])
+  
+    def get_batch_index(self, idx):
+        # Fetch a batch of index
+        return np.array(self.index[idx])
+
+    def get_batch_texts(self, idx):
+        # Fetch a batch of inputs
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+        batch_index = self.get_batch_index(idx)
+
+        return batch_texts, batch_y, batch_index
 
 
-# Load our best ML model -------------------------------------------------
 
-with open(f'{path_to_models_pol}/_{estimator}_{method}{tune_tag}', 'rb') as file:
-    model = dill.load(file)
-print('Model already trained')
+class BertClassifier(nn.Module):
+
+    def __init__(self, dropout=0.2):
+
+        super(BertClassifier, self).__init__()
+
+        self.bert = AutoModel.from_pretrained("m-polignano-uniba/bert_uncased_L-12_H-768_A-12_italian_alb3rt0")
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(768, 1) 
+        self.tanh = nn.Tanh()
+
+    def forward(self, input_id, mask):
+
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.tanh(linear_output)
+        return final_layer
+
+def mean_by_label(samples, labels):
+    df = pd.DataFrame(samples, labels, columns = ['pred'])
+    mean = df.groupby(df.index).pred.mean()
+    return list(mean.values), list(mean.index)
+
+def predict_from_model(model, data, batch_size = 32, bert_model = ''):
+
+    dataloader = torch.utils.data.DataLoader(data, batch_size = batch_size)
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    if use_cuda:
+
+        model = model.cuda()
+
+    total_preds = []
+    with torch.no_grad():
+    
+        for data_input, data_label, data_index in tqdm(dataloader):
+          
+            data_label = data_label.to(device)
+            mask = data_input['attention_mask'].to(device)
+            input_id = data_input['input_ids'].squeeze(1).to(device)
+            output = model(input_id, mask)
+            output = output.squeeze(-1)
+            preds = output.detach().cpu().numpy()
+            total_preds.extend(list(preds))
+
+    mean_out, mean_index = mean_by_label(total_preds, data.index)
+    total_preds = pd.DataFrame({'pred': total_preds, 'labels': data.labels}, index = data.index)
+    for i, m_index in enumerate(mean_index):
+      total_preds.loc[total_preds.index == mean_index[i], "pred"] = mean_out[i]
+    total_preds = total_preds.groupby(total_preds.index).mean()
+
+    return total_preds
+
+
+model = BertClassifier()
+torch.manual_seed(random_seed)
+
+model.load_state_dict(torch.load(f'{path_to_models}best_torch_{learning_rate}_{batch_size}_{epoch_num}{percentage_filter}.pt', map_location = 'cpu'))
 
 # Load the list of already predicted users
 try:
-    with open(f'{path_to_processed}pred_pol.pkl', 'rb') as f: 
-        pred_list = pickle.load(f)
-        print(f"URLs in dataset: {len(pred_list)}")
+    pred_list = pd.read_pickle(f"{path_to_processed}final_df{rank}.pkl.gz", compression = 'gzip')
+    print(f"Predictions done: {len(pred_list)}")
 except:
-    pred_list = []
+    pred_list = pd.DataFrame()
 
-# Work in batches
-maximum = pred.shape[0]
-minimum = len(pred_list)
-steps = 10000
-range_list = list(np.arange(minimum,maximum,steps))
-iter_count = 0
-
-for i in range(len(range_list)):
-    with open(f'{path_to_repo}log_pred.txt', 'w') as f:
-        f.write(f'Doing {iter_count} - out of {maximum}')
-    min_r = range_list[i]
-    try: 
-        max_r = range_list[i+1]
-    except: 
-        max_r = maximum # if we are out of range we use the list up to its maximum
-
-    batch_df = pred.loc[min_r:max_r]
-
-    print("Cleaning dataset")
-    batch_df["text_final"] = batch_df.tweet_text.apply(lambda x: text_prepare(x))
-
-    print("Count vectorizer")
-    X_train, x_pred, _ = vectorize_features(train, batch_df, method = method, include_val = False)
-
-    # Now predict over the whole dataset
-    preds = model.predict(x_pred)
-    pred_list = pred_list + list(preds)
-
-    iter_count += steps
-    with open(f'{path_to_repo}log_pred.txt', 'w') as f:
-        f.write(f'{iter_count} done - out of {maximum}')
-    if iter_count % 100000 == 0 or max_r == maximum:
-        with open(f'{path_to_processed}pred_pol.pkl', 'wb') as f: 
-            pickle.dump(pred_list, f)
+pred['final_polarization'] = 100
+batch_dataset = Dataset(pred)
+final_predictions = predict_from_model(model, batch_dataset, batch_size = 128, bert_model = '')
+final_predictions.reset_index(inplace = True, drop = True)
+pred['pred'] = final_predictions.pred
+final_predictions.to_pickle(f"{path_to_processed}final_df.pkl.gz", compression = 'gzip')
