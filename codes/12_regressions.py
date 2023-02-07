2db9ac2d671e0d47fabc054d36936985472deb64
LucaAdorni <luca.adorni.98@gmail.com>
2023-02-06 22:42:05 -0600

    feat: added double graphs

--- codes/12_regressions.py
+++ codes/12_regressions.py
@@ -54,7 +54,7 @@ os.makedirs(path_to_figure_odds, exist_ok = True)
 # 1. Define our main functions ------------------------------------------------------------
 
 # Drop NAs
-def do_logit(df, col, treat = 0, constant = 1):
+def do_logit(df, col, treat = 0, constant = 1, do_log = True):
     """
     Function to perform a logistic regression and give back a dataframe with the odds ratio
     df: our original dataset
@@ -64,15 +64,6 @@ def do_logit(df, col, treat = 0, constant = 1):
     """
     if treat == 1:
         df_copy = df.loc[df.treat == 1]
-        # # If treat == 1, we will also use dummies for our treated category
-        # df_reg = pd.concat([df[col], df.scree_name, pd.get_dummies(df.dist), df.treat, pd.get_dummies(df.dist_treat)], axis = 1)
-        # # Set to zero values not belonging to most_region
-        # for coltreat in df.dist_treat.unique():
-        #     df_reg.loc[df_reg.treat == 0, coltreat] = 0
-        # # Remove the first week of COVID-19
-        # df_reg.drop(columns = ['0 days', '0 days_treat'], inplace = True)
-        # # Define a variable to get the distance column
-        # dist_col = 'dist_treat'
     else:
         df_copy = df.copy()
     # Get dummies for each of our weeks
@@ -92,7 +83,10 @@ def do_logit(df, col, treat = 0, constant = 1):
     else:
         exog = sm.add_constant(df_reg.drop(columns = col))
     # Perform our logistic classification
-    logit_mod = sm.Logit(df_reg[col], exog)
+    if do_log == True:
+        logit_mod = sm.Logit(df_reg[col], exog)
+    else:
+        logit_mod = sm.OLS(df_reg[col], exog)
     results_logit = logit_mod.fit(cov_type = 'cluster', cov_kwds = {'groups': np.asarray(df_reg.index)})
     # Extract the parameters
     params = results_logit.params
@@ -103,8 +97,9 @@ def do_logit(df, col, treat = 0, constant = 1):
     conf.columns = ['5%', '95%', 'Odds Ratio', 'P-Values']
     conf_odds = ['5%', '95%', 'Odds Ratio']
     # Take the exponent of everything to get the Odds-Ratios
-    for col_exp in conf_odds:
-        conf[col] = np.exp(conf[col_exp])
+    if do_log == True:
+        for col_exp in conf_odds:
+            conf[col] = np.exp(conf[col_exp])
 
     conf = conf[conf.index.isin(df[dist_col].unique())]
     conf.reset_index(inplace = True, drop = False)
@@ -169,6 +164,7 @@ def time_plot(dataframe, column, tag = ""):
 
 df = pd.read_pickle(f"{path_to_processed}final_df_clean.pkl.gz", compression = 'gzip')
 df = df.loc[(df.week_start <= pd.datetime(2020, 12, 31))& (df.week_start > pd.datetime(2019, 12, 31))]
+df.drop(columns = 'tweet_text', inplace = True)
 
 # Get our distance from the beginning of the pandemic
 pandemic = pd.datetime(2020, 2, 24)
@@ -180,6 +176,9 @@ week_list = df[['dist', 'week_start']].drop_duplicates()
 df['treat'] = (df.regions == 'Lombardia') | (df.regions == "Veneto") | (df.regions == "Piemonte") 
 df['treat'].replace({False: 0, True:1}, inplace = True)
 df['dist_treat'] = df.dist + "_treat"
+
+# Get the total number of tweets as a control
+df['tot_activity'] = df.groupby('week_start').n_tweets.transform('sum')
 
 # Create also 0/1 for being in a certain group
 df = pd.concat([df, pd.get_dummies(df.polarization_bin)], axis = 1)
@@ -196,6 +195,81 @@ for y in iter_list:
     store_odds[y] = conf
 
 
+iter_emot = ['sentiment', 'anger', 'fear', 'joy', 'sadness']
+
+for y in iter_emot:
+    df[y] = df[y]/df.n_tweets
+    print(y)
+    conf = do_logit(df, y, treat = 0, do_log = False)
+    time_plot(conf, y, "")
+    store_odds[y] = conf
+
+
+# Get the n_tweets by extremists
+weights = df.loc[df.extremism_toright == 1]
+weights = weights.groupby(['week_start'], as_index = False).n_tweets.sum()
+
+store_odds['extremism_toright_ols'] = do_logit(df, 'extremism_toright', treat = 0, do_log = False)
+
+def double_lineplot(df1, df2, col1, col2, tag = ""):
+    """ Function to get a seaborn plot with two time series """
+    fig, ax = plt.subplots(figsize=(15, 10))
+    sns.lineplot(data=df1, x = 'week_start', y=col1
+                , ax=ax, label = col1, legend = False)
+    ax2 = ax.twinx()
+    sns.lineplot(data=df2, x = 'week_start', y=col2
+                , ax=ax2, label = col2, legend = False, color = 'darkorange')
+    plt.xlabel("Week Start")
+    ax.set_ylabel(col1)
+    ax2.set_ylabel(col2)
+    plt.axvline(pandemic, linewidth = 1, alpha = 1, color = 'red', linestyle = '--')
+    plt.axhline(0, linewidth = 1, alpha = 0.5, color = 'black', linestyle = '--')
+    h1, l1 = ax.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    ax.legend(h1+h2, l1+l2, loc='upper right')
+    save_fig(f'{path_to_figure_odds}{col1}_{col2}{tag}')
+
+double_lineplot(store_odds['extremism_toright_ols'], store_odds['anger'], col1 = 'extremism_toright', col2 = 'anger', tag="_ols")
+
+df['dist_extr'] = np.where(df.extremism_toright == 1, df.dist + "_extr", "0 days_extr")
+df_reg = pd.concat([df['anger'], df.scree_name, pd.get_dummies(df.dist), pd.get_dummies(df.dist_extr)], axis = 1)
+
+for col in df.dist_extr.unique():
+    df_reg[col] = df_reg[col]*df_reg.n_tweets
+# Drop the week when COVID-19 happened (i.e. last week of February when first cases appeared)
+df_reg.drop(columns = ['0 days', '0 days_extr'], inplace = True)
+# Define a variable to get the distance column
+dist_col = 'dist_extr'
+# Drop any NAs
+df_reg.dropna(axis = 0, inplace = True)
+# Set users as our index - we will then use it to cluster the standard errors
+df_reg.set_index(df_reg.scree_name, drop = True, inplace = True)
+df_reg.drop(columns = 'scree_name', inplace = True)
+
+
+exog = sm.add_constant(df_reg.drop(columns = 'anger'))
+logit_mod = sm.OLS(df_reg['anger'], exog)
+results_logit = logit_mod.fit(cov_type = 'cluster', cov_kwds = {'groups': np.asarray(df_reg.index)})
+# Extract the parameters
+params = results_logit.params
+conf = results_logit.conf_int()
+p_value_stars = ['***' if v <= 0.001 else '**' if v <= 0.01 else '*' if v <= 0.05 else '' for v in list(results_logit.pvalues)]
+conf['Odds Ratio'] = params
+conf['P-Values'] = p_value_stars
+conf.columns = ['5%', '95%', 'Odds Ratio', 'P-Values']
+conf_odds = ['5%', '95%', 'Odds Ratio']
+conf = conf[conf.index.isin(df[dist_col].unique())]
+conf.reset_index(inplace = True, drop = False)
+conf = pd.melt(conf, id_vars = ['index'], value_vars = ['5%', '95%', 'Odds Ratio'])
+conf.columns = [dist_col, 'variable', 'prova']
+conf['dist'] = conf.dist_extr.apply(lambda x: re.sub("_extr", "", x))
+conf = conf.merge(week_list, on = 'dist', how = 'left', indicator = True, validate = 'm:1')
+assert conf._merge.value_counts()['both'] == conf.shape[0]
+
+fig, ax = plt.subplots(figsize=(15, 10))
+sns.lineplot(data = conf, x = 'week_start', y = 'prova')
+plt.show()
+
 store_odds = {}
 # Iterate over all our otcomes and get the odds ratio graphs
 for y in iter_list:
@@ -207,3 +281,7 @@ for y in iter_list:
 
 # TBD - Invece che treated location, faccio Week + Week*Dosage (dosage = numero di morti per regione/settimana se trovo i dati)
 # Altrimenti uso come treated le 5 locatio con piu' morti nella prima ondata/primo anno? Check also time frame
+
+# Uso N/Tweets si o no?
+# Cerco il modo di fare correlati Anger e Extremist
+# Provo altre coppie?
