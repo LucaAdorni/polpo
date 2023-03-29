207a890d2a39cbcab7dcdd5f34a96d6d44f1c209
LucaAdorni <luca.adorni.98@gmail.com>
2023-03-28 21:52:01 -0500

    feat: LDA analysis

---
+++ codes/13_clean_topic.py
@@ -0,0 +1,250 @@
+###########################################################
+# TOPIC MODELING - GSDMM
+# Author: Luca Adorni
+# Date: March 2023
+###########################################################
+
+# 0. Setup -------------------------------------------------
+
+try:
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD # initialize communications
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    mpi_use = True
+except:
+    print("Not running on a cluster")
+    rank = 0
+    size = 1
+    mpi_use = False
+
+
+import re
+import nltk
+import gensim
+from nltk.stem import WordNetLemmatizer
+import spacy
+from nltk.corpus import stopwords
+import os
+import sys
+
+sys.path.append(f"{os.getcwd()}gsdmm/")
+
+import numpy as np
+import pandas as pd
+import pickle
+from tqdm import tqdm
+from nltk.stem.snowball import SnowballStemmer
+
+import warnings
+import datetime as dt
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+warnings.simplefilter("ignore")
+
+
+
+pd.options.display.max_columns = 200
+pd.options.display.max_rows = 1000
+pd.set_option('max_info_columns', 200)
+pd.set_option('expand_frame_repr', False)
+pd.set_option('expand_frame_repr', True)
+pd.set_option('max_colwidth',1000)
+pd.set_option('display.width',None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+# Setup Repository
+try:
+    # Setup Repository
+    with open("repo_info.txt", "r") as repo_info:
+        path_to_repo = repo_info.readline()
+except:
+    path_to_repo = f"{os.getcwd()}/polpo/"
+    sys.path.append(f"{os.getcwd()}/.local/bin") # import the temporary path where the server installed the module
+
+  
+print(path_to_repo)
+
+path_to_data = f"{path_to_repo}data/"
+path_to_raw = f"{path_to_data}raw/"
+path_to_links = f"{path_to_data}links/"
+path_to_processed = f"{path_to_data}processed/"
+path_to_figures = f"{path_to_repo}figures/"
+path_to_figure_odds = f"{path_to_figures}logit_res/"
+path_to_tables = f"{path_to_repo}tables/"
+path_to_ctm = f"{path_to_data}ctm/"
+path_to_lda = f"{path_to_data}lda/"
+path_to_gsdmm = f"{path_to_data}gsdmm/"
+
+os.makedirs(path_to_ctm, exist_ok=True)
+os.makedirs(path_to_lda, exist_ok=True)
+os.makedirs(path_to_gsdmm, exist_ok=True)
+
+# define a string of punctuation symbols
+punctuation = '!"$%&\'()*+,-./:;<=>?[\\]^_`{|}~•@'
+# Load italian stopwords
+stop_words = stopwords.words('italian')
+from numpy import loadtxt
+stop2 = loadtxt(f"{path_to_data}stopwords.txt", dtype = str, comments="#", delimiter=",", unpack=False)
+stop_words = stop_words + list(stop2)
+stop_words = list(set(stop_words))
+
+# Load SpaCy IT model
+nlp = spacy.load("it_core_news_sm")
+
+# we initialize our stemmer
+stemmer = SnowballStemmer("italian", ignore_stopwords=True)
+
+# PARAMETER ---------
+stem = True
+stem_tag = np.where(stem, "_stemm","")
+
+# 1. Define main functions --------------------------------------------------------
+
+# functions to clean tweets
+def remove_links(tweet):
+    """Takes a string and removes web links from it"""
+    tweet = re.sub(r'http\S+', '', tweet)   # remove http links
+    tweet = re.sub(r'bit.ly/\S+', '', tweet)  # remove bitly links
+    tweet = tweet.strip('[link]')   # remove [links]
+    tweet = re.sub(r'pic.twitter\S+','', tweet)
+    return tweet
+
+
+def remove_users(tweet):
+    """Takes a string and removes retweet and @user information"""
+    tweet = re.sub('(RT\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)  # remove re-tweet
+    tweet = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)  # remove tweeted at
+    return tweet
+
+
+def remove_hashtags(tweet):
+    """Takes a string and removes any hash tags"""
+    # tweet = re.sub('(#[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)  # remove hash tags
+    tweet = re.sub('#+', '', tweet)  # remove hash tags
+    return tweet
+
+def remove_keywords(tweet):
+    """Takes a string and removes the main keywords"""
+    tweet = re.sub('(coronavirus|covid19|covid-19|covid19italia|coronavid19|pandemia|corona|virus|covid)', '', tweet)  # remove hash tags
+    # vaccino keyword is wrongly recognized by NLP SpaCy model, need to replace it
+    tweet = re.sub('vaccino', "vaccinare", tweet)
+    return tweet
+
+def remove_av(tweet):
+    """Takes a string and removes AUDIO/VIDEO tags or labels"""
+    tweet = re.sub('VIDEO:', '', tweet)  # remove 'VIDEO:' from start of tweet
+    tweet = re.sub('AUDIO:', '', tweet)  # remove 'AUDIO:' from start of tweet
+    return tweet
+
+
+def tokenize(tweet):
+    """Returns tokenized representation of words in lemma form excluding stopwords"""
+    result = []
+    for token in gensim.utils.simple_preprocess(tweet):
+        if token not in stop_words \
+                and len(token) > 2:  # drops words with less than 3 characters
+            if stem == True:
+                result.append(stemmer.stem(token))
+            else:
+                result.append(nlp(token)[0].lemma_)
+    return result
+
+
+def preprocess_tweet(tweet):
+    """Main master function to clean tweets, stripping noisy characters, and tokenizing use lemmatization"""
+    tweet = remove_users(tweet)
+    tweet = remove_links(tweet)
+    tweet = remove_hashtags(tweet)
+    tweet = remove_av(tweet)
+    tweet = tweet.lower()  # lower case
+    tweet = remove_keywords(tweet)
+    tweet = re.sub('[' + punctuation + ']+', ' ', tweet)  # strip punctuation
+    tweet = re.sub('\s+', ' ', tweet)  # remove double spacing
+    tweet = re.sub('([0-9]+)', '', tweet)  # remove numbers
+    tweet_token_list = tokenize(tweet)  # apply lemmatization and tokenization
+    tweet = ' '.join(tweet_token_list)
+    return tweet
+
+
+def tokenize_tweets(df):
+    """Main function to read in and return cleaned and preprocessed dataframe.
+    This can be used in Jupyter notebooks by importing this module and calling the tokenize_tweets() function
+    Args:
+        df = data frame object to apply cleaning to
+    Returns:
+        pandas data frame with cleaned tokens
+    """
+
+    df['tokens'] = df.tweet_text.apply(preprocess_tweet)
+    num_tweets = len(df)
+    print('Complete. Number of Tweets that have been cleaned and tokenized : {}'.format(num_tweets))
+    return df
+
+
+
+from gensim.models import Phrases
+
+def prepare_tokens(docs, min_bigrams = 20):
+    bigram = Phrases(docs, min_count = min_bigrams)
+    for idx in range(len(docs)):
+        for token in bigram[docs[idx]]:
+            if '_' in token:
+                # Token is a bigram, add to document.
+                docs[idx].append(token)
+    return docs
+
+
+# 2. Load our dataset --------------------------------------------
+
+try:
+    final_df = pd.read_pickle(f"{path_to_processed}tweet_for_topic.pkl.gz", compression = 'gzip')
+    print("Loaded Data")
+except:
+    print("Creating data")
+    # Load our predicted dataset
+    df = pd.read_pickle(f"{path_to_processed}final_df_clean.pkl.gz", compression = 'gzip')
+    # Keep only the info we want
+    df = df[['week_start', 'scree_name', 'polarization_bin', 'prediction', 'extremism_toleft', 'extremism_toright', 'orient_change_toleft', 'orient_change_toright']]
+
+    # Load back the tweets
+    post_df = pd.read_csv(f"{path_to_raw}tweets_without_duplicates_regions_sentiment_demographics_topic_emotion.csv")
+    post_df['dates'] = pd.to_datetime(post_df.dates)
+    post_df['week_start'] = post_df['dates'].dt.to_period('W').dt.start_time
+    # Drop columns we do not need
+    post_df.drop(columns = ['tweet_ids', 'user_description' ,'locations', 'dates', 'place', 'polygon', 'user_id', 'profile_url', 'is_org'], inplace = True)
+
+    # Restrict to 2020
+    df = df.loc[(df.week_start <= pd.datetime(2020, 12, 31))& (df.week_start > pd.datetime(2019, 12, 31))]
+
+    # Now merge the two sets back
+    final_df = post_df.merge(df, how = 'inner', on = ['week_start', 'scree_name'], validate = 'm:1')
+
+    # Save it
+    final_df.to_pickle(f"{path_to_processed}tweet_for_topic.pkl.gz", compression = 'gzip')
+
+
+# Restrict to topics we care about
+final_df = final_df.loc[final_df.topic.isin(['politics', 'economics', 'health', 'vaccine'])]
+
+print("Process - Rank: %d -----------------------------------------------------"%rank)
+
+end = final_df.shape[0]
+batches = round(end/size)
+
+# if rank == size-1:
+#     print(f"Currently doing {batches*rank} to {end}")
+# else:
+#     print(f"Currently doing {batches*rank} to {batches*(rank+1)}")
+# final_df = final_df.iloc[(batches*rank):(batches*(rank+1))]
+final_df = tokenize_tweets(final_df)
+
+final_df.to_pickle(f"{path_to_processed}cleaned_gsdmm_tweets{rank}{stem_tag}.pkl.gz", compression = 'gzip')
+
+print(final_df.shape)
+
+# store = []
+# for i in range(200):
+#     store.append(pd.read_pickle(f"{path_to_processed}cleaned_gsdmm_tweets{i}{stem_tag}.pkl.gz", compression = 'gzip'))
+# store = pd.concat(store)
+
+# store.to_pickle(f"{path_to_processed}cleaned_gsdmm_tweets{stem_tag}.pkl.gz", compression = 'gzip')
---
+++ codes/14_lda_analysis.py
@@ -0,0 +1,398 @@
+###########################################################
+# TOPIC MODELING - LDA
+# Author: Luca Adorni
+# Date: March 2023
+###########################################################
+
+# 0. Setup -------------------------------------------------
+
+import re
+import os
+import sys
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+import dill
+import numpy as np
+import pandas as pd
+import pickle
+from tqdm import tqdm
+import warnings
+import datetime as dt
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+warnings.simplefilter("ignore")
+
+pd.options.display.max_columns = 200
+pd.options.display.max_rows = 1000
+pd.set_option('max_info_columns', 200)
+pd.set_option('expand_frame_repr', False)
+pd.set_option('expand_frame_repr', True)
+pd.set_option('max_colwidth',1000)
+pd.set_option('display.width',None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+# Setup Repository
+try:
+    # Setup Repository
+    with open("repo_info.txt", "r") as repo_info:
+        path_to_repo = repo_info.readline()
+except:
+    path_to_repo = f"{os.getcwd()}/polpo/"
+    sys.path.append(f"{os.getcwd()}/.local/bin") # import the temporary path where the server installed the module
+
+  
+print(path_to_repo)
+
+path_to_data = f"{path_to_repo}data/"
+path_to_raw = f"{path_to_data}raw/"
+path_to_links = f"{path_to_data}links/"
+path_to_processed = f"{path_to_data}processed/"
+path_to_figures = f"{path_to_repo}figures/"
+path_to_figure_odds = f"{path_to_figures}logit_res/"
+path_to_tables = f"{path_to_repo}tables/"
+path_to_ctm = f"{path_to_data}ctm/"
+path_to_lda = f"{path_to_data}lda/"
+path_to_sk_lda = f"{path_to_data}sk_lda/"
+
+os.makedirs(path_to_ctm, exist_ok=True)
+os.makedirs(path_to_lda, exist_ok=True)
+os.makedirs(path_to_sk_lda, exist_ok=True)
+os.makedirs(f"{path_to_sk_lda}clean/", exist_ok=True)
+
+# 1. Load our dataset --------------------------------------------
+
+# PARAMETER ---------
+stem = True
+stem_tag = np.where(stem, "_stemm","")
+period = 'periods'
+if period == 'months':
+    # Do LDA for the most relevant weeks during the pandemic
+    week_dict = {'feb': [2], 'mar': [3], 'apr': [4], 'may': [5], 
+                'june': [6], 'jul': [7],'aug': [8], 'sep': [9], 
+                'oct': [10],'nov': [11], 'dec': [12]}
+
+    n_topics = 10
+elif period == 'periods':
+    # Do LDA for the most relevant weeks during the pandemic
+    week_dict = {
+                'second_all': [7,8,9,10,11,12],
+                'middle': [5,6,7,8,9],
+                'all':[2,3,4,5,6,7,8,9,10,11,12],
+                'end_lock': [5,6],
+                'summer': [7,8,9], 
+                'second_lockdown': [10,11,12],
+                'first_lockdown': [2,3,4]}
+    n_topics = 20
+
+final_df = pd.read_pickle(f"{path_to_processed}cleaned_gsdmm_tweets{stem_tag}.pkl.gz", compression = 'gzip')
+# We want to have who switch vs who doesn't
+final_df['polarization_bin'] = np.where(final_df.extremism_toleft == 1, "extremism_toleft",
+                                        np.where(final_df.extremism_toright == 1, 'extremism_toright',
+                                        np.where(final_df.orient_change_toleft == 1, 'orient_change_toleft',
+                                        np.where(final_df.orient_change_toright == 1, 'orient_change_toright',
+                                        final_df.polarization_bin))))
+
+
+# Create also 0/1 for being in a certain group
+final_df = pd.concat([final_df, pd.get_dummies(final_df.polarization_bin)], axis = 1)
+
+# Define two lists for the different polarization measures we have
+pol_changes = ["extremism_toright",'center_right', "extremism_toleft","orient_change_toleft",
+            "orient_change_toright",'far_left', 'far_right', 'center_left', 
+                 'center']
+
+# 2. FUNCTIONS --------------------------------------------
+
+def vectorize_to_dataframe(df, vectorizer_obj):
+    """
+    Function to return a dataframe from our vectorizer results
+    """
+    df = pd.DataFrame(data = df.toarray(), columns = vectorizer_obj.get_feature_names())
+    return df
+
+
+def display_topics(model, feature_names, no_top_words):
+    """ Function to print all the main words from each topic """
+    for topic_idx, topic in enumerate(model.components_):
+        print("Topic %d:" % (topic_idx))
+        print(" ".join([feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]]))
+        print("-"*10)
+
+def top_tweets(model, topic_dct, feature_names, no_top_words):
+    """ Function to get a dataframe with each topic, its main words and an example tweet"""
+    topic_store = []
+    tweet_store = []
+    word_store = []
+    for topic_idx, topic in enumerate(model.components_):
+        topic_store.append(f"topic_{topic_idx}")
+        tweet_store.append(df.iloc[topic_dct[f'topic_{topic_idx}'].idxmax(axis = 0)].tweet_text)
+        word_store.append([feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]])
+    return pd.DataFrame([topic_store, word_store, tweet_store], index = ['Topic','Words', 'Tweets']).T
+
+# define a function that saves figures
+def save_fig(fig_id, tight_layout=True):
+    # The path of the figures folder ./Figures/fig_id.png (fig_id is a variable that you specify 
+    # when you call the function)
+    path = os.path.join(os.getcwd(), "Figures", fig_id + ".pdf") 
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='pdf', dpi=300)
+
+# 3. LDA -----------------------------------------------------------------------------------------------------------
+
+# ALL --------------------------------------------------------------------------------------------------------------
+
+# PARAMETERS ----------------------
+key_w = 'all'
+week_date = week_dict[key_w]
+pol = 'extremism_toright'
+tag = '_pol'
+# ----------------------------------
+
+# Subset our dataframe
+sub_df = final_df.loc[(final_df.week_start.dt.month.isin(week_date))&(final_df['polarization_bin'] == pol)]
+# Train a topic for politic tweets
+pol_df = sub_df.loc[(sub_df.topic == 'politics')|(sub_df.topic == 'economics')]
+# And for health tweets
+vac_df = sub_df.loc[(sub_df.topic == 'health')|(sub_df.topic == 'vaccine')]
+iter_dict = {'_vacc': vac_df, '_pol': pol_df}
+
+# Clean our dataframe and compute our DTM
+df = iter_dict[tag].copy()
+vectorizer = CountVectorizer(ngram_range=(1,2), min_df=5)
+df_token = vectorizer.fit_transform(df.tokens)
+df_token = vectorize_to_dataframe(df_token, vectorizer)
+
+try:
+    with open(f'{path_to_sk_lda}lda_{key_w}{tag}{pol}{stem_tag}_{n_topics}', 'rb') as file: # and save the fitted model
+        lda = dill.load(file)
+        print("Model already trained")
+except:
+    print("Training LDA")
+    lda = LatentDirichletAllocation(n_components = n_topics, random_state = 42, n_jobs = -1)
+    lda.fit(df_token)
+    with open(f'{path_to_sk_lda}lda_{key_w}{tag}{pol}{stem_tag}_{n_topics}', 'wb') as file: # and save the fitted model
+        dill.dump(lda, file)
+
+# Extract its components
+lda_components = pd.DataFrame(lda.components_, columns = df_token.columns).T
+lda_components.to_csv(f'{path_to_sk_lda}lda_{key_w}{tag}{pol}{stem_tag}{n_topics}.csv')
+
+# Print its top words
+no_top_words = 20
+display_topics(lda, vectorizer.get_feature_names(), no_top_words)
+
+# Create a DocumentXTopic Matrix
+topic_dct = lda.transform(df_token)
+topic_dct = pd.DataFrame(topic_dct, columns = [f"topic_{i}" for i in range(n_topics)])
+# Get the predicted topic
+topic_dct['pred'] = topic_dct.idxmax(axis = 1)
+df['pred'] = topic_dct['pred'].tolist()
+
+
+# Get the Top Tweets
+top_df = top_tweets(lda, topic_dct, vectorizer.get_feature_names(), no_top_words)
+
+# 0: Conte unable to manage the lockdown
+# 1: Mixed topic, both against UE's help (Fabio Rampelli's episode) and talking about Zaia's Veneto
+# 2: Government unable to manage the pandemic
+# 3: Against the government, democracy is harmed
+# 4: Xenophobia, both migrants + Roma's rom episode
+# 5: Against the "virus" terrorism, echoing words of Italian doctors
+# 6: Conspiracies - Government knew already
+# 7: Against Arcuri, head of Protezione Civile (i.e. govmnt mismanagement)
+# 8: Mixed Xenophobia + Conspiracies (i.e. Government already knew - predominant topic)
+# 9: Mix between government fault + dissatisfaction for closed churces
+# 10: Conspiracies - Dictatorship + Wuhan
+# 11: Against lockdowns + Cina's help
+# 12: Dictatorship (Global Government)
+# 13: Xenophobia (COVID-affected migrants trade) + Economic crisis
+# 14: Xenophobia (gov bus with Tunisian people) + Econ crisis
+# 15: Economic crisis
+# 16: Other, Travaglio complaining against the government
+# 17: Xenophobia, migrants
+# 18: Conspiracy - Wuhan
+# 19: Against government fear for COVID
+
+
+# Create macro-topics
+topic_dict = {'Xenophobia': [4, 13, 14, 17],
+              'Conspiracies': [3, 6, 8, 10, 12, 18],
+              'Government': [0, 1, 2, 5, 7, 9, 19],
+              'Other': [11, 15, 16]}
+
+for k,v in topic_dict.items():
+    topic_dict[k] = [f"topic_{i}" for i in v]
+
+df['macro'] = ""
+top_df['Macro-Topic'] = ""
+for k in topic_dict.keys():
+    df.loc[df.pred.isin(topic_dict[k]), 'macro'] = k
+    top_df.loc[top_df.Topic.isin(topic_dict[k]), 'Macro-Topic'] = k
+
+# Cleanup and export to LaTex
+top_df = top_df[['Topic', 'Macro-Topic', 'Tweets']]
+top_df['Topic'] = top_df['Topic'].apply(lambda x: re.sub("topic_", "Topic ", x))
+top_df.to_latex(f"{path_to_sk_lda}clean/top_tweets{key_w}{tag}{pol}{stem_tag}{n_topics}.tex")
+
+# Now generate the plot over time
+df['n_obs'] = 1
+plot_df = df.groupby(['week_start', 'macro'], as_index = False).n_obs.sum()
+plot_df['tot'] = plot_df.groupby('week_start').n_obs.transform('sum')
+plot_df['n_obs'] = plot_df.n_obs/plot_df.tot
+plot_df = plot_df.loc[plot_df.macro != 'Other']
+
+# Balance our panel data
+# Get Unique macro-topics
+uid = plot_df['macro'].unique()
+# Get an array of week_start and transform it to a dataframe
+dates = np.array([plot_df.week_start.unique() for x in range(len(uid))])
+balanced_panel = pd.DataFrame(dates).T
+balanced_panel.columns = uid
+# Bring everything to long format and merge it back
+balanced_panel = pd.melt(balanced_panel, var_name = 'macro', value_name = 'week_start')
+rebalanced_data = pd.merge(balanced_panel, plot_df, how='left', on=['macro', 'week_start']).fillna(0)
+
+# Plot over time the macro-topics
+fig, ax = plt.subplots(figsize=(15, 10))
+sns.lineplot(data = rebalanced_data, x = 'week_start', y = 'n_obs', 
+             hue = 'macro', markers = True,
+             markersize = 10, style = 'macro', dashes = ['', '', ''], ax = ax)
+sns.despine()
+plt.ylabel('% of Tweets', fontsize = 28)
+plt.xlabel('Weeks', fontsize = 28)
+plt.yticks(fontsize = 20)
+plt.xticks(fontsize = 20)
+plt.legend(loc = 'upper right', fontsize = 28)
+plt.ylim(0,1)
+ax.xaxis.set_major_formatter(
+    mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
+
+save_fig(f'{path_to_sk_lda}clean/figure_{key_w}{tag}{pol}{stem_tag}{n_topics}')
+
+
+
+# ALL --------------------------------------------------------------------------------------------------------------
+
+# PARAMETERS ----------------------
+key_w = 'all'
+week_date = week_dict[key_w]
+pol = 'extremism_toright'
+tag = '_vacc'
+# ----------------------------------
+
+# Clean our dataframe and compute our DTM
+df = iter_dict[tag].copy()
+vectorizer = CountVectorizer(ngram_range=(1,2), min_df=5)
+df_token = vectorizer.fit_transform(df.tokens)
+df_token = vectorize_to_dataframe(df_token, vectorizer)
+
+try:
+    with open(f'{path_to_sk_lda}lda_{key_w}{tag}{pol}{stem_tag}_{n_topics}', 'rb') as file: # and save the fitted model
+        lda = dill.load(file)
+        print("Model already trained")
+except:
+    print("Training LDA")
+    lda = LatentDirichletAllocation(n_components = n_topics, random_state = 42, n_jobs = -1)
+    lda.fit(df_token)
+    with open(f'{path_to_sk_lda}lda_{key_w}{tag}{pol}{stem_tag}_{n_topics}', 'wb') as file: # and save the fitted model
+        dill.dump(lda, file)
+
+# Extract its components
+lda_components = pd.DataFrame(lda.components_, columns = df_token.columns).T
+lda_components.to_csv(f'{path_to_sk_lda}lda_{key_w}{tag}{pol}{stem_tag}{n_topics}.csv')
+
+# Print its top words
+no_top_words = 20
+display_topics(lda, vectorizer.get_feature_names(), no_top_words)
+
+# Create a DocumentXTopic Matrix
+topic_dct = lda.transform(df_token)
+topic_dct = pd.DataFrame(topic_dct, columns = [f"topic_{i}" for i in range(n_topics)])
+# Get the predicted topic
+topic_dct['pred'] = topic_dct.idxmax(axis = 1)
+df['pred'] = topic_dct['pred'].tolist()
+
+
+# Get the Top Tweets
+top_df = top_tweets(lda, topic_dct, vectorizer.get_feature_names(), no_top_words)
+
+# 0: Pandemic spread + Sigonella fake news (spread virus)
+# 1: Xenophobia
+# 2: Discussion on masks
+# 3: COVID-19 Cases
+# 4: Plasma to cure COVID
+# 5: China/Conspiracies
+# 6: China/Conspiracies
+# 7: Cases
+# 8: Cases
+# 9: Cases
+# 10: Cases
+# 11: Cases
+# 12: Conspiracies
+# 13: Against closures
+# 14: Conspiracies
+# 15: Lockdown measures
+# 16: Cases/Measures
+# 17: Cases
+# 18: Vaccines
+# 19: Covid studies
+
+# Create macro-topics
+topic_dict = {'Xenophobia': [1],
+              'Conspiracies': [5, 6, 12, 14],
+              'Lockdown': [2, 13, 15],
+              'Other': [0, 3, 4, 7, 8, 9, 10, 11, 16, 17, 18, 19]}
+
+for k,v in topic_dict.items():
+    topic_dict[k] = [f"topic_{i}" for i in v]
+
+df['macro'] = ""
+top_df['Macro-Topic'] = ""
+for k in topic_dict.keys():
+    df.loc[df.pred.isin(topic_dict[k]), 'macro'] = k
+    top_df.loc[top_df.Topic.isin(topic_dict[k]), 'Macro-Topic'] = k
+
+# Cleanup and export to LaTex
+top_df = top_df[['Topic', 'Macro-Topic', 'Tweets']]
+top_df['Topic'] = top_df['Topic'].apply(lambda x: re.sub("topic_", "Topic ", x))
+top_df.to_latex(f"{path_to_sk_lda}clean/top_tweets{key_w}{tag}{pol}{stem_tag}{n_topics}.tex")
+
+# Now generate the plot over time
+df['n_obs'] = 1
+plot_df = df.groupby(['week_start', 'macro'], as_index = False).n_obs.sum()
+plot_df['tot'] = plot_df.groupby('week_start').n_obs.transform('sum')
+plot_df['n_obs'] = plot_df.n_obs/plot_df.tot
+plot_df = plot_df.loc[plot_df.macro != 'Other']
+
+# Balance our panel data
+# Get Unique macro-topics
+uid = plot_df['macro'].unique()
+# Get an array of week_start and transform it to a dataframe
+dates = np.array([plot_df.week_start.unique() for x in range(len(uid))])
+balanced_panel = pd.DataFrame(dates).T
+balanced_panel.columns = uid
+# Bring everything to long format and merge it back
+balanced_panel = pd.melt(balanced_panel, var_name = 'macro', value_name = 'week_start')
+rebalanced_data = pd.merge(balanced_panel, plot_df, how='left', on=['macro', 'week_start']).fillna(0)
+
+# Plot over time the macro-topics
+fig, ax = plt.subplots(figsize=(15, 10))
+sns.lineplot(data = rebalanced_data, x = 'week_start', y = 'n_obs', 
+             hue = 'macro', markers = True,
+             markersize = 10, style = 'macro', dashes = ['', '', ''], ax = ax)
+sns.despine()
+plt.ylabel('% of Tweets', fontsize = 28)
+plt.xlabel('Weeks', fontsize = 28)
+plt.yticks(fontsize = 20)
+plt.xticks(fontsize = 20)
+plt.legend(loc = 'upper right', fontsize = 28)
+plt.ylim(0,1)
+ax.xaxis.set_major_formatter(
+    mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
+
+save_fig(f'{path_to_sk_lda}clean/figure_{key_w}{tag}{pol}{stem_tag}{n_topics}')
